eb345b494ed64fa36431d377bc0db349fc6b9e78
Sibaja1 <104949837+Sibaja1@users.noreply.github.com>
2023-06-29 00:56:25 -0600

    Add files via upload

---
+++ RemotingExample.Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using RemotingExample.Server;
+using System;
+using System.Runtime.Remoting;
+
+namespace RemotingExample.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IRemoteCalculator remoteCalculator = (IRemoteCalculator)Activator.GetObject(
+            typeof(IRemoteCalculator), "tcp://localhost:1234/RemoteCalculator");
+
+            Console.WriteLine("Enter two numbers:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int sum = remoteCalculator.Add(a, b);
+            int difference = remoteCalculator.Subtract(a, b);
+            int product = remoteCalculator.Multiply(a, b);
+            double quotient = remoteCalculator.Divide(a, b);
+
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Difference: {difference}");
+            Console.WriteLine($"Product: {product}");
+            Console.WriteLine($"Quotient: {quotient}");
+
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ RemotingExample.Server/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+
+namespace RemotingExample.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpChannel channel = new TcpChannel(1234);
+            ChannelServices.RegisterChannel(channel, false);
+
+            RemotingConfiguration.RegisterWellKnownServiceType(
+                typeof(RemoteCalculator), "RemoteCalculator", WellKnownObjectMode.Singleton);
+
+            Console.WriteLine("Server is running. Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
